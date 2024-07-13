aca9dfb3a9edc4321fd54bfae5ac72706718b308
BazylevDV <dmbazylev@gmail.com>
2024-07-13 08:06:47 +0500

    Add files via upload

---
+++ Module2,3,1.py
@@ -0,0 +1,26 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+count = 0
+while count < len(my_list):
+    count = int(input())
+if count > 0:
+    print(count)  #
+    count += 1
+elif count <= 0:
+    count += 1
+
+        # This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Привет, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Мир')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
