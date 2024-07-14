c62ebda1fbc222b6b34578cc29b9a1ab64d602c0
BazylevDV <dmbazylev@gmail.com>
2024-07-14 09:47:05 +0500

    Add files via upload

---
+++ module2.4.1.py
@@ -0,0 +1,35 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+for number in numbers:
+    if number < 2:
+        not_primes.append(number)
+    else:
+        is_prime = True
+        for i in range(2, int(number**0.5) + 1):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime :
+            primes.append(number)
+        else:
+            not_primes.append(number)
+not_primes.remove (1)
+print("primes:", primes)
+print("not_primes:", not_primes)
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
